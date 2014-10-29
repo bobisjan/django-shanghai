2ab2a4929b733e399cc53670f03b5e24d052062a
Jan Bobisud <me@bobisjan.com>
2014-10-29 20:51:49 +0100

    Allow to `DELETE` linked
    JSONAPI allows to delete only a to-one linked relationship.

--- shanghai/actions/linked.py
+++ shanghai/actions/linked.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponseNotFound
 from django.db import models
 
+from shanghai.http import HttpResponseNoContent
+
 
 class LinkedMixin(object):
 
@@ -12,15 +14,18 @@ class LinkedMixin(object):
         except models.ObjectDoesNotExist:
             return None
         else:
-            relationship = self.relationship_for(self.link)
+            relationship = self.get_linked_relationship()
 
             return relationship.get_from(obj)
 
     def get_linked_serializer(self):
-        relationship = self.relationship_for(self.link)
+        relationship = self.get_linked_relationship()
         resource = self.api.resource_for(relationship.target)
 
         return resource.serializer
+
+    def get_linked_relationship(self):
+        return self.relationship_for(self.link)
 
     def get_linked(self):
         data = self.get_linked_data()
@@ -30,6 +35,18 @@ class LinkedMixin(object):
 
         serializer = self.get_linked_serializer()
         return self.response(data, serializer=serializer)
+
+    def delete_linked(self):
+        obj = self.get_object_data()
+        relationship = self.get_linked_relationship()
+
+        if not obj or not relationship.is_belongs_to():
+            return HttpResponseNotFound()
+
+        relationship.set_to(obj, None)
+        update_fields = [relationship.name]
+        obj.save(update_fields=update_fields)
+        return HttpResponseNoContent()
 
 
 class LinkedObjectMixin(object):
