0c52e468e135a3c8996ba50cdd714a14248e6d85
Jan Bobisud <me@bobisjan.com>
2014-11-02 15:56:16 +0100

    Release v0.0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-shanghai',
-    version='0.0.4',
+    version='0.0.5',
     packages=['shanghai'],
     license='MIT License',
     description='A Django app to provide JSON API resources.',
