57883ffb82cfaff8d4ad2f0cb8c90da2f2d4662f
Jan Bobisud <me@bobisjan.com>
2014-11-04 13:11:30 +0100

    Add `DELETE` linked tests

--- tests/integration/actions/linked.py
+++ tests/integration/actions/linked.py
@@ -43,3 +43,35 @@ class GetLinkedHasManyTestCase(TestCase):
         self.assertIsNotNone(articles)
         self.assertIsInstance(articles, list)
         self.assertTrue(len(articles) > 0)
+
+
+class DeleteLinkedBelongsToTestCase(TestCase):
+
+    def test_app_should_delete_category_on_article(self):
+        response = self.client.delete('/api/articles/1/links/category')
+
+        self.assertEqual(response.status_code, 204)
+
+        response = self.client.get('/api/articles/1/links/category')
+        categories = response.document.get('categories', None)
+        self.assertIsNone(categories)
+
+        response = self.client.get('/api/articles/1')
+        articles = response.document.get('articles', None)
+
+        self.assertIsNone(articles.get('links').get('categories'))
+
+
+class DeleteHasManyTestCase(TestCase):
+
+    def test_app_should_not_delete_articles_on_category(self):
+        response = self.client.get('/api/categories/1/links/articles')
+        before_articles = response.document.get('articles')
+
+        response = self.client.delete('/api/categories/1/links/articles')
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.get('/api/categories/1/links/articles')
+        after_articles = response.document.get('articles')
+
+        self.assertListEqual(after_articles, before_articles)
